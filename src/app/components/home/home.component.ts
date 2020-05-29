caca46bfade90c4fb3ba46038da9aa0159fb9286
Hamza <muhammadhamza1311@gmail.com>
2020-05-29 22:17:41 +0500

    clean up

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
+/* eslint-disable no-unused-vars */
 import { Component, OnInit } from '@angular/core'
 import { Router } from '@angular/router'
-import { AngularFireAuth } from '@angular/fire/auth'
 import { DiaryService } from '../../services/diary.service'
 import { map } from 'rxjs/operators'
 import { DiaryItem } from '../../models/DiaryItem'
+/* eslint-enable no-unused-vars */
 
 @Component({
     selector: 'app-home',
@@ -13,19 +14,17 @@ import { DiaryItem } from '../../models/DiaryItem'
 export class HomeComponent implements OnInit {
     items;
 
-    constructor(private router: Router, private diaryService: DiaryService) {
-    }
+    // eslint-disable-next-line no-unused-vars
+    constructor(private router: Router, private diaryService: DiaryService) { }
 
     async ngOnInit() {
         const ref = await this.diaryService.getAllItems()
-        this.items = ref.snapshotChanges().pipe(map(docs => docs.map(doc =>{
-            const data = doc.payload.doc.data() as DiaryItem
-            const id = doc.payload.doc.id
-            return { id, ...data }
-        })))
-        // ref.valueChanges({ idField: 'yeah' }).subscribe(items => {
-        //     this.items = items
-        // })
+        this.items = ref.snapshotChanges().pipe(
+            map(docs => docs.map(doc =>{
+                const data = doc.payload.doc.data() as DiaryItem
+                const id = doc.payload.doc.id
+                return { id, ...data }
+            }))
+        )
     }
-
 }
