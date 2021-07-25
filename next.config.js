af23c87e59ddf22ccb4ead02969d1bc9ba0c2263
Hamza <muhammadhamza1311@gmail.com>
2021-07-25 20:22:24 +0500

    try netlify hosting

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    target: "serverless",
+    webpack: (config, { isServer }) => {
+        // Important: return the modified config
+        if (!isServer) {
+            config.resolve.fallback.fs = false
+            config.resolve.fallback.child_process = false
+            config.resolve.fallback.request = false
+            config.resolve.fallback.net = false
+            config.resolve.fallback.worker_threads = false
+            config.resolve.fallback.tls = false
+        }
+        return config
+    },
 }
