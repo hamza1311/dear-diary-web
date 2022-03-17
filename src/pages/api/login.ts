0834cce09e9a2993e0db542004bd4ae67157bacf
Muhammad Hamza <muhammadhamza1311@gmail.com>
2022-03-18 00:25:26 +0500

    Debug Vercel function timeouts (#5)
    * add logging
    
    * update deps

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -6,8 +6,11 @@ import {NextApiRequest, NextApiResponse} from "next"; // the module you created 
 initAuth()
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    console.log('login handler')
     try {
+        console.time('login handler: setAuthCookies')
         await setAuthCookies(req, res)
+        console.timeEnd('login handler: setAuthCookies')
     } catch (e) {
         console.error(e)
         return res.status(500).json({error: 'Unexpected error.'})
